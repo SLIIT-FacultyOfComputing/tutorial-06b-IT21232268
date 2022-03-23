c58e43501a529dd514e88f32bb4f4e42a54e10e4
De Silva E.G.M.H <99390811+IT21232268@users.noreply.github.com>
2022-03-23 04:24:40 +0000

    I have completed Exercise.

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,28 @@
 #include "Box.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
 
 // Implement setters and getters
-
+void Box::setlength(double l){
+  length=l;
+  }
+void Box::setwidth(double w){
+  width=w;
+  }
+void Box::setheigth(double h){
+  heigth=h;
+  }
+double Box::getlength(){
+   return length;
+ }
+double Box::getwidth(){
+  return width;
+}
+double Box::getheigth(){
+  return heigth;
+}
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+  return lenght*width*heigth;
 }
--- Box.h
+++ Box.h
@@ -5,7 +5,14 @@ class Box {
        int height;
     public:
        // write prototypes of setters for length, width and height
+      void setlength(double l);
+      void setwidth(double w);
+      void setheigth(double h);
+
        // write prototypes of getters for length, width and height
- 
+       double getlength;
+       double getwidth;
+       double getheigth;
+
        int calcVolume();
 };
